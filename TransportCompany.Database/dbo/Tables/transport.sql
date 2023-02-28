dd0106b52341a6c26a0becf8074e17ee0f8a73f5
ElkinAndrey <andreyridbir@gmail.com>
2023-02-28 21:41:06 +0500

    Добавил таблицу с категориями транспорта

--- TransportCompany.Database/dbo/Tables/transport.sql
+++ TransportCompany.Database/dbo/Tables/transport.sql
@@ -1,11 +1,15 @@
 ﻿CREATE TABLE [dbo].[transport] (
-    [transport_id] BIGINT        NOT NULL,
-    [series]       NVARCHAR (10) NOT NULL,
-    [number]       NVARCHAR (10) NOT NULL,
-    [region_code]  NVARCHAR (10) NOT NULL,
-    [start]        DATETIME      NOT NULL,
-    [end]          DATETIME      NOT NULL,
-    [mileage]      INT           NOT NULL,
-    PRIMARY KEY CLUSTERED ([transport_id] ASC)
+    [transport_id]          BIGINT        NOT NULL,
+    [series]                NVARCHAR (10) NOT NULL,
+    [number]                NVARCHAR (10) NOT NULL,
+    [region_code]           NVARCHAR (10) NOT NULL,
+    [start]                 DATETIME      NOT NULL,
+    [end]                   DATETIME      NULL,
+    [mileage]               INT           NOT NULL,
+    [transport_category_id] SMALLINT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([transport_id] ASC),
+    FOREIGN KEY ([transport_category_id]) REFERENCES [dbo].[transport_category] ([transport_category_id])
 );
+
+
 
---
+++ TransportCompany.Database/dbo/Tables/transport_category.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[transport_category] (
+    [transport_category_id] SMALLINT       NOT NULL,
+    [name]                  NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([transport_category_id] ASC)
+);
+
