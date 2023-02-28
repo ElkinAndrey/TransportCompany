f607e0f8d5332f3625c2bc8e752ee2e43d50c3b6
ElkinAndrey <andreyridbir@gmail.com>
2023-02-28 21:08:43 +0500

    Создание таблицы 'Транспорт' в базе данных

---
+++ TransportCompany.Database/dbo/Tables/transport.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[transport] (
+    [transport_id] BIGINT        NOT NULL,
+    [series]       NVARCHAR (10) NOT NULL,
+    [number]       NVARCHAR (10) NOT NULL,
+    [region_code]  NVARCHAR (10) NOT NULL,
+    [start]        DATETIME      NOT NULL,
+    [end]          DATETIME      NOT NULL,
+    [mileage]      INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([transport_id] ASC)
+);
+
