b864c13d4cd03c1481cccc85954e7d132a4bb991
ElkinAndrey <andreyridbir@gmail.com>
2023-03-01 21:49:17 +0500

    Добавление таблицы в которой будет храниться информация о том, какие уникальные характеристики есть у каждой категории транспорта

---
+++ TransportCompany.Database/dbo/Tables/transport_category_transport_property.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[transport_category_transport_property] (
+    [transport_property_id] BIGINT   NOT NULL,
+    [transport_category_id] SMALLINT NOT NULL,
+    PRIMARY KEY CLUSTERED ([transport_property_id] ASC, [transport_category_id] ASC),
+    FOREIGN KEY ([transport_category_id]) REFERENCES [dbo].[transport_category] ([transport_category_id]) ON DELETE CASCADE,
+    FOREIGN KEY ([transport_property_id]) REFERENCES [dbo].[transport_property] ([transport_property_id]) ON DELETE CASCADE
+);
+
