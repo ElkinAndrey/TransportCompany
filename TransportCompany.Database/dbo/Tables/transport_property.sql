1fbc4e362fa2146b97bab2542b3cdf3b8a6ed97a
ElkinAndrey <andreyridbir@gmail.com>
2023-03-01 21:37:52 +0500

    Добавление таблицы с уникальными характеристиками транспорта

---
+++ TransportCompany.Database/dbo/Tables/transport_property.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[transport_property] (
+    [transport_property_id] BIGINT         NOT NULL,
+    [name]                  NVARCHAR (MAX) DEFAULT ('') NOT NULL,
+    [data_type_id]          SMALLINT       NOT NULL,
+    PRIMARY KEY CLUSTERED ([transport_property_id] ASC),
+    FOREIGN KEY ([data_type_id]) REFERENCES [dbo].[data_type] ([data_type_id]) ON DELETE CASCADE
+);
+
