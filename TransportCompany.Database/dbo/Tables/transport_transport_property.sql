219580aa5f3b27fbac35dce61fbc411fe1e047ab
ElkinAndrey <andreyridbir@gmail.com>
2023-03-01 21:44:05 +0500

    Добавление таблицы для связи многие ко многим у транспорта и уникальной характеристики транспорта

---
+++ TransportCompany.Database/dbo/Tables/transport_transport_property.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[transport_transport_property] (
+    [transport_property_id] BIGINT         NOT NULL,
+    [transport_id]          BIGINT         NOT NULL,
+    [content]               NVARCHAR (MAX) DEFAULT ('') NULL,
+    PRIMARY KEY CLUSTERED ([transport_property_id] ASC, [transport_id] ASC),
+    FOREIGN KEY ([transport_id]) REFERENCES [dbo].[transport] ([transport_id]) ON DELETE CASCADE,
+    FOREIGN KEY ([transport_property_id]) REFERENCES [dbo].[transport_property] ([transport_property_id]) ON DELETE CASCADE
+);
+
