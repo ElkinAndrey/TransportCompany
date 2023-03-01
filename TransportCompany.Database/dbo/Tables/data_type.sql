3898ca655c0257b79eb6a133db54c0cea5bac656
ElkinAndrey <andreyridbir@gmail.com>
2023-03-01 21:22:50 +0500

    Добавление таблицы с типами данных

---
+++ TransportCompany.Database/dbo/Tables/data_type.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[data_type] (
+    [data_type_id] SMALLINT       NOT NULL,
+    [name]         NVARCHAR (MAX) DEFAULT ('') NOT NULL,
+    PRIMARY KEY CLUSTERED ([data_type_id] ASC)
+);
+
