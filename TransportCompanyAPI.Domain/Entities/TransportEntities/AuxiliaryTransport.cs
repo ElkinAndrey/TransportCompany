f7da1ec9e33943a4bdd29f44bdd709cf2fd807b4
ElkinAndrey <andreyridbir@gmail.com>
2023-03-02 14:20:18 +0500

    Добавил сущность вспомогательный транспорт

---
+++ TransportCompanyAPI.Domain/Entities/TransportEntities/AuxiliaryTransport.cs
@@ -0,0 +1,13 @@
+﻿namespace TransportCompanyAPI.Domain.Entities.TransportEntities
+{
+    /// <summary>
+    /// Вспомогательный транспорт
+    /// </summary>
+    public class AuxiliaryTransport : Transport
+    {
+        /// <summary>
+        /// Высота
+        /// </summary>
+        public double Height { get; set; }
+    }
+}
