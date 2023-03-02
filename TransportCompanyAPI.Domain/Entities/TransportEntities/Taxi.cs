bd77159a770625c44cd0e8578cb0236de44fef43
ElkinAndrey <andreyridbir@gmail.com>
2023-03-02 14:21:35 +0500

    Добавил сущность такси

---
+++ TransportCompanyAPI.Domain/Entities/TransportEntities/Taxi.cs
@@ -0,0 +1,13 @@
+﻿namespace TransportCompanyAPI.Domain.Entities.TransportEntities
+{
+    /// <summary>
+    /// Такси
+    /// </summary>
+    public class Taxi : Transport
+    {
+        /// <summary>
+        /// Количество сидячих мест
+        /// </summary>
+        public int NumberSeats { get; set; }
+    }
+}
