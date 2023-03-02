9a83e0ada12e95df10f92fdfbeb7c77966f3dbf5
ElkinAndrey <andreyridbir@gmail.com>
2023-03-02 14:20:59 +0500

    Добавил сущность грузовой транспорт

---
+++ TransportCompanyAPI.Domain/Entities/TransportEntities/FreightTransport.cs
@@ -0,0 +1,18 @@
+﻿namespace TransportCompanyAPI.Domain.Entities.TransportEntities
+{
+    /// <summary>
+    /// Грузовой транспорт
+    /// </summary>
+    public class FreightTransport : Transport
+    {
+        /// <summary>
+        /// Грузоподьемность (в килограммах)
+        /// </summary>
+        public int LoadCapacity { get; set; }
+
+        /// <summary>
+        /// Высота
+        /// </summary>
+        public double Height { get; set; }
+    }
+}
