a1175a4bcd8d657bdec7716be86972e32427fa6d
ElkinAndrey <andreyridbir@gmail.com>
2023-03-02 14:07:49 +0500

    Добавил SQL функцию для получения уникальных характеристик по Id транспорта

---
+++ TransportCompany.Database/dbo/Functions/GetPropertyByTransportId.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION GetPropertyByTransportId (
+	@transportId NVARCHAR(MAX)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT 
+		[transport_property].[name],
+		[transport_transport_property].[content]
+	FROM (
+		SELECT * 
+		FROM [transport_transport_property] 
+		WHERE [transport_id] = @transportId
+	) AS [transport_transport_property]
+	LEFT JOIN [transport_property] ON 
+		[transport_transport_property].[transport_property_id]=[transport_property].[transport_property_id]
+);
