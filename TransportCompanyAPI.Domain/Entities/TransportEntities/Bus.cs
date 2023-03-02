8d6c6098fa35fdb0750c4f7c4412834dcfe717d8
ElkinAndrey <andreyridbir@gmail.com>
2023-03-02 14:20:39 +0500

    Добавил сущность автобус

---
+++ TransportCompanyAPI.Domain/Entities/TransportEntities/Bus.cs
@@ -0,0 +1,23 @@
+﻿namespace TransportCompanyAPI.Domain.Entities.TransportEntities
+{
+    /// <summary>
+    /// Автобус
+    /// </summary>
+    public class Bus : Transport
+    {
+        /// <summary>
+        /// Количество сидячих мест
+        /// </summary>
+        public int NumberSeats { get; set; }
+
+        /// <summary>
+        /// Количество стоячих мест
+        /// </summary>
+        public int NumberStandingPlaces { get; set; }
+
+        /// <summary>
+        /// Количество мест для инвалидов
+        /// </summary>
+        public int NumberPlacesForDisabled { get; set; }
+    }
+}
