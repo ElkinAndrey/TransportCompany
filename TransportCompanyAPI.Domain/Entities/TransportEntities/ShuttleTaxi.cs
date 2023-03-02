822905a5307a2b4294ce0eecea064a2dae77808b
ElkinAndrey <andreyridbir@gmail.com>
2023-03-02 14:21:20 +0500

    Добавил сущность маршрутное такси

---
+++ TransportCompanyAPI.Domain/Entities/TransportEntities/ShuttleTaxi.cs
@@ -0,0 +1,13 @@
+﻿namespace TransportCompanyAPI.Domain.Entities.TransportEntities
+{
+    /// <summary>
+    /// Маршрутное такси
+    /// </summary>
+    public class ShuttleTaxi : Transport
+    {
+        /// <summary>
+        /// Количество сидячих мест
+        /// </summary>
+        public int NumberSeats { get; set; }
+    }
+}
