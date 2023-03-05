a095d73fd4c66a55635b33258ebaddff19d76db8
ElkinAndrey <andreyridbir@gmail.com>
2023-03-05 12:38:33 +0500

    Добавление SQL функции для получения количества транспорта

---
+++ TransportCompany.Database/dbo/Functions/GetTransportCount.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION GetTransportCount ()
+RETURNS @TL TABLE(
+	[count]   BIGINT NOT NULL
+)
+AS
+BEGIN
+	DECLARE @count BIGINT;
+
+	SET @count = (
+		SELECT COUNT([transport].[transport_id])
+		FROM [transport]
+	)
+
+	INSERT INTO @TL ([count]) 
+	VALUES (@count)
+	RETURN
+END;
