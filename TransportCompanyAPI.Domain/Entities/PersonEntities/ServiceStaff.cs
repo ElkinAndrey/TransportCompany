d9bc9d17f6561ab7d8fd4e11e938c12e1f7d0800
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 13:11:36 +0500

    Добавил сущьность 'Обслуживающий персонал'

---
+++ TransportCompanyAPI.Domain/Entities/PersonEntities/ServiceStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompanyAPI.Domain.Entities.PersonEntities
+{
+    /// <summary>
+    /// Обслуживающий персонал
+    /// </summary>
+    public abstract class ServiceStaff : Person
+    {
+        /// <summary>
+        /// Бригада
+        /// </summary>
+        // public Brigade Brigade { get; set; }
+    }
+}
