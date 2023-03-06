797e7a995989149153e7e34452f76f292df80e1a
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 21:21:00 +0500

    Добавил таблицу с людьми

---
+++ TransportCompany.Database/dbo/Tables/person.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[person] (
+    [person_id]          BIGINT         IDENTITY (1, 1) NOT NULL,
+    [name]               NVARCHAR (MAX) DEFAULT ('') NOT NULL,
+    [surname]            NVARCHAR (MAX) DEFAULT ('') NOT NULL,
+    [patronymic]         NVARCHAR (MAX) DEFAULT ('') NOT NULL,
+    [start]              DATETIME       NOT NULL,
+    [end]                DATETIME       NULL,
+    [person_position_id] SMALLINT       NOT NULL,
+    [country_id]         SMALLINT       NOT NULL,
+    [brand_id]           BIGINT         NOT NULL,
+    PRIMARY KEY CLUSTERED ([person_id] ASC),
+    CHECK ([start]<[end]),
+    FOREIGN KEY ([person_position_id]) REFERENCES [dbo].[person_position] ([person_position_id]) ON DELETE CASCADE
+);
+
