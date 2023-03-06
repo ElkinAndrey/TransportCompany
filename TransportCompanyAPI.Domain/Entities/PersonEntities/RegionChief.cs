5b02b4f85bad6909af2757437a1ccc3206fb5dc5
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 13:14:38 +0500

    Добавил сущьность 'Начальник участка'

---
+++ TransportCompanyAPI.Domain/Entities/PersonEntities/RegionChief.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompanyAPI.Domain.Entities.PersonEntities
+{
+    /// <summary>
+    /// Начальник участка
+    /// </summary>
+    public class RegionChief : Person
+    {
+        /// <summary>
+        /// Участок
+        /// </summary>
+        // public Region Region { get; set; }
+    }
+}
