5bc79d5485b14bbc3875c2b653742f5d44350274
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 21:36:13 +0500

    Добавил таблицу, в которой будет храниться информация о том, какие уникальные характеристики есть у должности человека

---
+++ TransportCompany.Database/dbo/Tables/person_position_person_property.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[person_position_person_property] (
+    [person_property_id] BIGINT   NOT NULL,
+    [person_position_id] SMALLINT NOT NULL,
+    PRIMARY KEY CLUSTERED ([person_property_id] ASC, [person_position_id] ASC),
+    FOREIGN KEY ([person_position_id]) REFERENCES [dbo].[person_position] ([person_position_id]) ON DELETE CASCADE,
+    FOREIGN KEY ([person_property_id]) REFERENCES [dbo].[person_property] ([person_property_id]) ON DELETE CASCADE
+);
+
