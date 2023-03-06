a322ce6297b00d2f74c11f52b642d1f8739a3cab
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 13:16:18 +0500

    Добавил сущьность 'Бригадир'

---
+++ TransportCompanyAPI.Domain/Entities/PersonEntities/Foreman.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompanyAPI.Domain.Entities.PersonEntities
+{
+    /// <summary>
+    /// Бригадир
+    /// </summary>
+    public class Foreman : Person
+    {
+        /// <summary>
+        /// Бригада
+        /// </summary>
+        // public Brigade Brigade { get; set; }
+    }
+}
