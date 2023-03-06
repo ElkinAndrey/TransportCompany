ceb6aaf567ec60a179479735a054436f3ca1104c
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 13:15:47 +0500

    Добавил сущьность 'Слесарь'

---
+++ TransportCompanyAPI.Domain/Entities/PersonEntities/Locksmith.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompanyAPI.Domain.Entities.PersonEntities
+{
+    /// <summary>
+    /// Слесарь
+    /// </summary>
+    public class Locksmith : ServiceStaff
+    {
+
+    }
+}
