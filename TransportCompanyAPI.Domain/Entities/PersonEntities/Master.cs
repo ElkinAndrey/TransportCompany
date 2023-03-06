a5dee977070d407d30974be0183a51ad0b4a22ed
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 13:15:21 +0500

    Добавил сущьность 'Мастер'

---
+++ TransportCompanyAPI.Domain/Entities/PersonEntities/Master.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompanyAPI.Domain.Entities.PersonEntities
+{
+    /// <summary>
+    /// Мастер
+    /// </summary>
+    public class Master : Person
+    {
+        /// <summary>
+        /// Мастерская мастера
+        /// </summary>
+        // public Workshop Workshop { get; set; }
+    }
+}
