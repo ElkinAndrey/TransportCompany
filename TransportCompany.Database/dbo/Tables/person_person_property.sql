84434da3e14566d2008e7d2c873ef6233354fc9b
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 21:34:06 +0500

    Добавил таблицу для связи людей и уникальных характеристик людей

---
+++ TransportCompany.Database/dbo/Tables/person_person_property.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[person_person_property] (
+    [person_property_id] BIGINT         NOT NULL,
+    [person_id]          BIGINT         NOT NULL,
+    [content]            NVARCHAR (MAX) DEFAULT ('') NULL,
+    PRIMARY KEY CLUSTERED ([person_property_id] ASC, [person_id] ASC),
+    FOREIGN KEY ([person_id]) REFERENCES [dbo].[person] ([person_id]) ON DELETE CASCADE,
+    FOREIGN KEY ([person_property_id]) REFERENCES [dbo].[person_property] ([person_property_id]) ON DELETE CASCADE
+);
+
--- TransportCompany.Database/dbo/Tables/person_property.sql
+++ TransportCompany.Database/dbo/Tables/person_property.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[person_property] (
-    [person_property_id] BIGINT         IDENTITY (1, 1) NOT NULL,
+    [person_property_id] BIGINT         NOT NULL,
     [name]               NVARCHAR (MAX) DEFAULT ('') NOT NULL,
     [translation]        NVARCHAR (MAX) DEFAULT ('') NOT NULL,
     [data_type_id]       SMALLINT       NOT NULL,
     PRIMARY KEY CLUSTERED ([person_property_id] ASC),
     FOREIGN KEY ([data_type_id]) REFERENCES [dbo].[data_type] ([data_type_id]) ON DELETE CASCADE
 );
+
+
 
