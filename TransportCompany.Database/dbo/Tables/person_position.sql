f9817be922dc893d75bb050323650e17fdccad52
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 21:12:44 +0500

    Добавил таблицу с должностями человека

---
+++ TransportCompany.Database/dbo/Tables/person_position.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[person_position] (
+    [person_position_id] SMALLINT       NOT NULL,
+    [name]               NVARCHAR (MAX) DEFAULT ('') NOT NULL,
+    PRIMARY KEY CLUSTERED ([person_position_id] ASC)
+);
+
