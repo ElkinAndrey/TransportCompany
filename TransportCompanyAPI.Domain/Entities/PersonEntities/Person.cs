a2665efec17c95a8f8fb67270ce9ed4caafc8546
ElkinAndrey <andreyridbir@gmail.com>
2023-03-06 13:09:34 +0500

    Добавил сущьность 'Человек'

---
+++ TransportCompanyAPI.Domain/Entities/PersonEntities/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCompanyAPI.Domain.Entities.PersonEntities
+{
+    /// <summary>
+    /// Человек
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Уникальный Id человека
+        /// </summary>
+        public long PersonId { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string Patronymic { get; set; }
+
+        /// <summary>
+        /// Должность человека
+        /// </summary>
+        public string PersonPosition { get; set; }
+
+        /// <summary>
+        /// Дата приема на работу
+        /// </summary>
+        public DateTime HireDate { get; set; }
+
+        /// <summary>
+        /// Дата увольнения
+        /// </summary>
+        public DateTime? DismissalDate { get; set; }
+    }
+}
