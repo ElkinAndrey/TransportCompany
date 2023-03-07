a2e2bc94497973711d9c2706f5763937ea541430
ElkinAndrey <andreyridbir@gmail.com>
2023-03-07 13:53:28 +0500

    Добавил SQL функцию для получения должностей человека

---
+++ TransportCompany.Database/dbo/Functions/GetPersonPositions.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION GetPersonPositions ()
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT 
+		[person_position].[person_position_id] AS [position_id],
+		[person_position].[name] AS [name]
+	FROM 
+		[person_position]
+);
