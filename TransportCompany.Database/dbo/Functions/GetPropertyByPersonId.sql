03ebaf0660d2ed0e858bc4d744da1f80a93b6328
ElkinAndrey <andreyridbir@gmail.com>
2023-03-07 14:31:31 +0500

    Добавил функцию для получения характеристик человека по Id

---
+++ TransportCompany.Database/dbo/Functions/GetPropertyByPersonId.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION GetPropertyByPersonId (
+	@personId BIGINT
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT 
+		[person_property].[name],
+		[person_person_property].[content]
+	FROM (
+		SELECT * 
+		FROM [person_person_property] 
+		WHERE [person_id] = @personId
+	) AS [person_person_property]
+	LEFT JOIN [person_property] ON 
+		[person_person_property].[person_property_id]=[person_property].[person_property_id]
+);
