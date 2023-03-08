d518c315c0d65774acb561b7fe692c7ce6a6ef83
ElkinAndrey <andreyridbir@gmail.com>
2023-03-08 12:28:30 +0500

    Добавил SQL функцию для получиения участков

---
+++ TransportCompany.Database/dbo/Functions/GetRegions.sql
@@ -0,0 +1,29 @@
+﻿CREATE FUNCTION GetRegions (
+	@regionId BIGINT = 0
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT 
+		[region].[region_id]		AS [region_id],
+		[region].[name]				AS [region_name],
+		
+		[person].[person_id]		AS [region_chief_id],
+		[person].[name]				AS [region_chief_name],
+		[person].[surname]			AS [region_chief_surname],
+		[person].[patronymic]		AS [region_chief_patronymic],
+		[person].[start]			AS [region_chief_start],
+		[person].[end]				AS [region_chief_end],
+		[person_position].[name]	AS [region_chief_position]
+	FROM (
+		SELECT *
+		FROM [region]
+		WHERE 
+			(@regionId = 0 OR @regionId = [region].[region_id])
+	) AS [region]
+	LEFT JOIN [person] ON 
+		[person].[person_id]=[region].[region_chief_id]
+	LEFT JOIN [person_position] ON 
+		[person].[person_position_id]=[person_position].[person_position_id]
+);
