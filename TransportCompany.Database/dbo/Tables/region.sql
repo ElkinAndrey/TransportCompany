584f381674ba3d703a8db5816c1409991304cceb
ElkinAndrey <andreyridbir@gmail.com>
2023-03-08 10:57:13 +0500

    Добавил таблицу с участками

---
+++ TransportCompany.Database/dbo/Tables/region.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[region] (
+    [region_id]       BIGINT         IDENTITY (1, 1) NOT NULL,
+    [name]            NVARCHAR (MAX) DEFAULT ('') NOT NULL,
+    [region_chief_id] BIGINT         NOT NULL,
+    PRIMARY KEY CLUSTERED ([region_id] ASC),
+    FOREIGN KEY ([region_chief_id]) REFERENCES [dbo].[person] ([person_id]) ON DELETE CASCADE
+);
+
