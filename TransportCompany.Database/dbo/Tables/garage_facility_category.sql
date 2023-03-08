3617a8e8c5d30872372fb650b40d81f25f20876c
ElkinAndrey <andreyridbir@gmail.com>
2023-03-08 10:59:33 +0500

    Добавил таблицу с объектами гаражного хозяйства

---
+++ TransportCompany.Database/dbo/Tables/garage_facility_category.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[garage_facility_category] (
+    [garage_facility_category_id] BIGINT         IDENTITY (1, 1) NOT NULL,
+    [name]                        NVARCHAR (MAX) DEFAULT ('') NOT NULL,
+    PRIMARY KEY CLUSTERED ([garage_facility_category_id] ASC)
+);
+
