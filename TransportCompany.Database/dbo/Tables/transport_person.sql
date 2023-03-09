4c02eb586b286c7d20cf8dffe44974002edbf4e9
ElkinAndrey <andreyridbir@gmail.com>
2023-03-09 14:45:47 +0500

    Добавил базу данных для связи водителей и транспорта

---
+++ TransportCompany.Database/dbo/Tables/transport_person.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[transport_person] (
+    [transport_id] BIGINT NOT NULL,
+    [person_id]    BIGINT NOT NULL,
+    PRIMARY KEY CLUSTERED ([transport_id] ASC, [person_id] ASC),
+    FOREIGN KEY ([person_id]) REFERENCES [dbo].[person] ([person_id]) ON DELETE CASCADE,
+    FOREIGN KEY ([transport_id]) REFERENCES [dbo].[transport] ([transport_id]) ON DELETE CASCADE
+);
+
