c29880ae04bfb8085f6edab874dd95a0237ddea4
ElkinAndrey <andreyridbir@gmail.com>
2023-03-09 20:10:56 +0500

    Добавил страницу с бригадой

---
+++ transport-company-app/src/components/pages/BrigadePage.jsx
@@ -0,0 +1,191 @@
+import React, { useEffect, useReducer } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useFetching } from "./../../hooks/useFetching";
+import Subordination from "./../../api/subordination";
+import TableLink from "./../forms/tableLink/TableLink";
+
+const BrigadePage = () => {
+  const dataFetchedRef = useReducer(false);
+  const params = useParams();
+  let history = useNavigate();
+  let [brigade, setBrigade] = useState({
+    brigadeId: "",
+    name: "",
+    foreman: {
+      personId: "",
+      name: "",
+      surname: "",
+      patronymic: "",
+      hireDate: "",
+      dismissalDate: "",
+    },
+    workshop: {
+      workshopId: "",
+      name: "",
+      master: {
+        personId: "",
+        name: "",
+        surname: "",
+        patronymic: "",
+        hireDate: "",
+        dismissalDate: "",
+      },
+      garageFacility: {
+        garageFacilityId: "",
+        street: "",
+        house: "",
+        category: "",
+      },
+    },
+    serviceStaffs: [],
+  });
+  let [count, setCount] = useState({
+    regionCount: "",
+    workshopCount: "",
+    brigadeCount: "",
+    personCount: "",
+  });
+
+  const [fetchBrigade, isBrigadeLoading, brigadeError] = useFetching(
+    async (brigadeId) => {
+      const response = await Subordination.getBrigade(brigadeId);
+      setBrigade(response.data);
+    }
+  );
+
+  const [fetchCount, isCountLoading, countError] = useFetching(
+    async (params) => {
+      const response = await Subordination.getSubordinationCount(params);
+      setCount(response.data);
+    }
+  );
+
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    fetchBrigade(params.brigadeId);
+    fetchCount({ brigadeId: params.brigadeId });
+  }, []);
+
+  const redirectPerson = (personId) => {
+    history(`/person/${personId}`);
+  };
+
+  const redirectWorkshop = (workshopId) => {
+    history(`/workshop/${workshopId}`);
+  };
+
+  return (
+    <div>
+      <h1>Бригада</h1>
+      <div>Уникальный Id бригады : {brigade.brigadeId}</div>
+      <div>Название бригады : {brigade.name}</div>
+
+      <h2>Персонал на предприятии</h2>
+      <div>Количество персонала : {count.personCount}</div>
+      <div>
+        Общее число человек : {1 + count.brigadeCount + count.personCount}
+      </div>
+
+      <h2>Мастерская</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Уникальный Id</th>
+            <th>Название</th>
+            <th>ФИО мастера</th>
+            <th>Адрес</th>
+            <th>Категория</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            key={brigade.workshop.workshopId}
+            className={"stringTable"}
+            style={{ position: "relative" }}
+          >
+            <td>{brigade.workshop.workshopId}</td>
+            <td>{brigade.workshop.name}</td>
+            <td>
+              {brigade.workshop.master.surname} {brigade.workshop.master.name}{" "}
+              {brigade.workshop.master.patronymic}
+            </td>
+            <td>{brigade.workshop.garageFacility.address}</td>
+            <td>{brigade.workshop.garageFacility.category}</td>
+            <TableLink to={`/workshop/${brigade.workshop.workshopId}`} />
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Начальник бригады</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Уникальный Id</th>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Отчество</th>
+            <th>Дата приема на работу</th>
+            <th>Дата увольнения</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={"stringTable"} style={{ position: "relative" }}>
+            <td>{brigade.foreman.personId}</td>
+            <td>{brigade.foreman.name}</td>
+            <td>{brigade.foreman.surname}</td>
+            <td>{brigade.foreman.patronymic}</td>
+            <td>{brigade.foreman.hireDate}</td>
+            <td>
+              {brigade.foreman.dismissalDate === null
+                ? "Не уволен"
+                : brigade.foreman.dismissalDate}
+            </td>
+            <TableLink to={`/person/${brigade.foreman.personId}`} />
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Работники</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Уникальный Id</th>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Отчество</th>
+            <th>Должность</th>
+            <th>Дата приема на работу</th>
+            <th>Дата увольнения</th>
+          </tr>
+        </thead>
+        <tbody>
+          {brigade.serviceStaffs.map((person) => (
+            <tr
+              key={person.personId}
+              className={"stringTable"}
+              style={{ position: "relative" }}
+            >
+              <td>{person.personId}</td>
+              <td>{person.name}</td>
+              <td>{person.surname}</td>
+              <td>{person.patronymic}</td>
+              <td>{person.personPosition}</td>
+              <td>{person.hireDate}</td>
+              <td>
+                {person.dismissalDate === null
+                  ? "Не уволен"
+                  : person.dismissalDate}
+              </td>
+              <TableLink to={`/person/${person.personId}`} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BrigadePage;
--- transport-company-app/src/router/index.js
+++ transport-company-app/src/router/index.js
@@ -4,6 +4,7 @@ import TransportsPage from './../components/pages/TransportsPage';
 import RegionsPage from './../components/pages/RegionsPage';
 import RegionPage from './../components/pages/RegionPage';
 import WorkshopPage from './../components/pages/WorkshopPage';
+import BrigadePage from '../components/pages/BrigadePage';
 
 export const routes = [
   { path: "/", element: <Navigate to="/transport" />, exact: true },
@@ -13,4 +14,5 @@ export const routes = [
   { path: "/region", element: <RegionsPage />, exact: true },
   { path: "/region/:regionId", element: <RegionPage />, exact: true },
   { path: "/workshop/:workshopId", element: <WorkshopPage />, exact: true },
+  { path: "/brigade/:brigadeId", element: <BrigadePage />, exact: true },
 ];
