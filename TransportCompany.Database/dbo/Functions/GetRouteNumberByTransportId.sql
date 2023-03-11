ebe11d639d0148fb148845793acb3b7cb3286e42
ElkinAndrey <andreyridbir@gmail.com>
2023-03-11 13:01:25 +0500

    Добавил SQL функцию для получения номера маршрута по Id транспорта

---
+++ TransportCompany.Database/dbo/Functions/GetRouteNumberByTransportId.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION GetRouteNumberByTransportId (
+	@transportId BIGINT 
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT [number] AS [route_number]
+	FROM (
+		SELECT TOP(1) [route_id]
+		FROM [transport_route]
+		WHERE [transport_route].[transport_id] = @transportId
+	) AS [transport_route]
+	LEFT JOIN [route] ON
+		[route].[route_id] = [transport_route].[route_id]
+);
