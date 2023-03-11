3e508afc83c9e960eff642c1d5acf7353441821b
ElkinAndrey <andreyridbir@gmail.com>
2023-03-11 12:43:40 +0500

    Добавил талбицу для связи транспорта и маршрута

---
+++ TransportCompany.Database/dbo/Tables/transport_route.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[transport_route] (
+    [transport_id] BIGINT NOT NULL,
+    [route_id]     BIGINT NOT NULL,
+    PRIMARY KEY CLUSTERED ([transport_id] ASC, [route_id] ASC),
+    FOREIGN KEY ([route_id]) REFERENCES [dbo].[route] ([route_id]) ON DELETE CASCADE,
+    FOREIGN KEY ([transport_id]) REFERENCES [dbo].[transport] ([transport_id]) ON DELETE CASCADE
+);
+
