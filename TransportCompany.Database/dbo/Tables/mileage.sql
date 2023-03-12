21d9eaacb11bdefa389177044215b04977e28800
ElkinAndrey <andreyridbir@gmail.com>
2023-03-12 15:12:34 +0500

    Добавил таблицу с пробегом

---
+++ TransportCompany.Database/dbo/Tables/mileage.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[mileage] (
+    [mileage_id]    UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [fixed_mileage] INT              NOT NULL,
+    [date]          DATETIME         NOT NULL,
+    [transport_id]  BIGINT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([mileage_id] ASC),
+    FOREIGN KEY ([transport_id]) REFERENCES [dbo].[transport] ([transport_id]) ON DELETE CASCADE
+);
+
