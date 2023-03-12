9371fdb2568fd80514af9384c3f18b9d94f8cd16
ElkinAndrey <andreyridbir@gmail.com>
2023-03-12 13:52:00 +0500

    Добавил SQL функцию для получения списка грузоперевозок

---
+++ TransportCompany.Database/dbo/Functions/GetCargoTransportations.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION GetCargoTransportations (
+	@length BIGINT,
+	@transportId NVARCHAR(MAX),
+	@start NVARCHAR(MAX) = '',
+	@end NVARCHAR(MAX) = ''
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT TOP(@length)
+		[cargo_transportation_id],
+		[price],
+		[additional_information],
+		[start],
+		[end]
+	FROM [cargo_transportation]
+	WHERE 
+		(@transportId = [transport_id]) AND
+		(@start = ''	OR	CONVERT(DATETIME, @start) <= [end])	AND		
+		(@end = ''		OR	CONVERT(DATETIME, @end) >= [end])
+);
