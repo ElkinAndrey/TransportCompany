5abe354d9f8ed62be3efe6b9e6fb3d6325176c29
ElkinAndrey <andreyridbir@gmail.com>
2023-03-12 13:43:17 +0500

    Добавил таблицу с грузоперевозками

---
+++ TransportCompany.Database/dbo/Tables/cargo_transportation.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[cargo_transportation] (
+    [cargo_transportation_id] BIGINT         IDENTITY (1, 1) NOT NULL,
+    [price]                   MONEY          DEFAULT ((0)) NOT NULL,
+    [additional_information]  NVARCHAR (MAX) DEFAULT ('') NOT NULL,
+    [start]                   DATETIME       NOT NULL,
+    [end]                     DATETIME       NULL,
+    [transport_id]            BIGINT         NOT NULL,
+    PRIMARY KEY CLUSTERED ([cargo_transportation_id] ASC),
+    FOREIGN KEY ([transport_id]) REFERENCES [dbo].[transport] ([transport_id]) ON DELETE CASCADE
+);
+
