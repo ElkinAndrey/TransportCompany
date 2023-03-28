b6b9e326140e5f9b0db133b561b5e45b6575aef4
ElkinAndrey <andreyridbir@gmail.com>
2023-03-28 16:44:38 +0500

    Добавил SQL функцию для получения количества и стоимости ремонтов по id транспорта за указанный период

---
+++ TransportCompany.Database/dbo/Functions/GetRepairInformationByTransportId.sql
@@ -0,0 +1,23 @@
+﻿CREATE FUNCTION GetRepairInformationByTransportId (
+	@transportId BIGINT = 0,
+	@start NVARCHAR(MAX) = '',
+	@end NVARCHAR(MAX) = ''
+)
+RETURNS TABLE
+AS
+RETURN
+(		
+	SELECT 
+		COUNT ([repair].[repair_id]) AS [count],
+		ISNULL(SUM ([repair].[price]), 0) AS [price]
+	FROM (
+		SELECT * 
+		FROM [transport]
+		WHERE @transportId = 0 OR [transport].[transport_id] = @transportId
+	) AS [transport]
+	INNER JOIN [repair] ON
+		[repair].[transport_id] = [transport].[transport_id]
+	WHERE 
+		(@start = ''	OR	CONVERT(DATETIME, @start) <= [repair].[end])	AND		
+		(@end = ''		OR	CONVERT(DATETIME, @end) >= [repair].[end])
+);
