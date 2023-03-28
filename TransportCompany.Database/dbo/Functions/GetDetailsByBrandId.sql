ede7d7ccf5dc307b2a627b1fbe6fb3af9854a867
ElkinAndrey <andreyridbir@gmail.com>
2023-03-28 19:09:00 +0500

    Добавил SQL функцию для получения количества и стоимости ремонтов по марке транспорта за указанный период

---
+++ TransportCompany.Database/dbo/Functions/GetDetailsByBrandId.sql
@@ -0,0 +1,40 @@
+﻿
+CREATE FUNCTION GetDetailsByBrandId (
+	@brandId SMALLINT = 0,
+	@start NVARCHAR(MAX) = '',
+	@end NVARCHAR(MAX) = '',
+	@details [smallint_list] READONLY
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT 
+		[detail].[name] AS [name],
+		[delail_count].[count] AS [count]
+	FROM (
+		SELECT 
+			[repair_person_detail_action].[detail_id],
+			COUNT(*) AS [count]
+
+		FROM (
+			SELECT [repair].[repair_id]
+			FROM (
+				SELECT * 
+				FROM [transport]
+				WHERE @brandId = 0 OR [transport].[brand_id] = @brandId
+			) AS [transport]
+			INNER JOIN [repair] ON
+				[repair].[transport_id] = [transport].[transport_id]
+			WHERE 
+				(@start = ''	OR	CONVERT(DATETIME, @start) <= [repair].[end])	AND		
+				(@end = ''		OR	CONVERT(DATETIME, @end) >= [repair].[end])
+		) AS [repair]
+		INNER JOIN [repair_person_detail_action] ON 
+			[repair_person_detail_action].[repair_id] = [repair].[repair_id]
+		WHERE [repair_person_detail_action].[detail_id] IN (SELECT [smallint] FROM @details)
+		GROUP BY [repair_person_detail_action].[detail_id]
+	) AS [delail_count]
+	LEFT JOIN [detail] ON
+		[detail].[detail_id] = [delail_count].[detail_id]
+);
