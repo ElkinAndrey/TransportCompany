a68f1174e90095c0cc2f719ded71717a8d38a409
ElkinAndrey <andreyridbir@gmail.com>
2023-03-28 16:25:28 +0500

    Добавил SQL функцию для получения количества и стоимости ремонтов по категории за указанный период

---
+++ TransportCompany.Database/dbo/Functions/GetRepairInformationByCategoryId.sql
@@ -0,0 +1,23 @@
+﻿CREATE FUNCTION GetRepairInformationByCategoryId (
+	@categoryId SMALLINT = 0,
+	@start NVARCHAR(MAX) = '',
+	@end NVARCHAR(MAX) = ''
+)
+RETURNS TABLE
+AS
+RETURN
+(		
+	SELECT 
+		COUNT ([repair].[repair_id]) AS [count],
+		ISNULL(SUM ([repair].[price]), 0) AS [price]
+	FROM (
+		SELECT [transport].[transport_id]
+		FROM [transport]
+		WHERE (@categoryId = 0 OR [transport].[transport_category_id] = @categoryId)
+	) AS [transport]
+	INNER JOIN [repair] ON
+		[repair].[transport_id] = [transport].[transport_id]
+	WHERE 
+		(@start = ''	OR	CONVERT(DATETIME, @start) <= [end])	AND		
+		(@end = ''		OR	CONVERT(DATETIME, @end) >= [end])
+);
