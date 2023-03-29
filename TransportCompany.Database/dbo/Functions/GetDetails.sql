41df45ee851090a11f2ae75948fae5a3f27df7de
ElkinAndrey <andreyridbir@gmail.com>
2023-03-29 10:39:56 +0500

    Добавил SQL фукнцию для получения списка деталей

---
+++ TransportCompany.Database/dbo/Functions/GetDetails.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION GetDetails ()
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT 
+		[detail].[detail_id] AS [detail_id],
+		[detail].[name] AS [name]
+	FROM 
+		[detail]
+);
--- TransportCompany.Database/dbo/Functions/GetRepairByPersonId.sql
+++ TransportCompany.Database/dbo/Functions/GetRepairByPersonId.sql
@@ -14,17 +14,7 @@ RETURN
 		[repair].[repair_id] AS [repair_id],
 		[repair].[price] AS [repair_full_price],
 		[repair].[start] AS [repair_start],
-		[repair].[end] AS [repair_end],
-
-		[transport].[transport_id] AS [transport_id],
-		[transport_category].[name] AS [transport_category],
-		[transport].[series] AS [transport_series],
-		[transport].[number] AS [transport_number],
-		[transport].[region_code] AS [transport_region_code],
-		[country].[country_code] AS [transport_country_code],
-		[country].[deciphering_country] AS [transport_deciphering_country],
-		[transport].[start] AS [transport_start],
-		[transport].[end] AS [transport_end]
+		[repair].[end] AS [repair_end]
 	FROM (
 		SELECT 
 			[repair].[repair_id] AS [repair_id],
@@ -45,14 +35,8 @@ RETURN
 			(@start = ''				OR	CONVERT(DATETIME, @start) <= [repair].[end])		AND
 			(@end = ''					OR	CONVERT(DATETIME, @end) >= [repair].[end])
 	) AS [repair]
-	LEFT JOIN [transport] ON
-		[repair].[transport_id] = [transport].[transport_id]
 	LEFT JOIN [action] ON
 		[action].[action_id] = [repair].[action_id]
 	LEFT JOIN [detail] ON
 		[detail].[detail_id] = [repair].[detail_id]
-	LEFT JOIN [transport_category] ON
-		[transport].[transport_category_id] = [transport_category].[transport_category_id]
-	LEFT JOIN [country] ON
-		[transport].[country_id] = [country].[country_id]
 );
